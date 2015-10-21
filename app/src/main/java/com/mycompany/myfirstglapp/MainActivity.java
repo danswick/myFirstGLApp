6772642f017aaa60ede2304404f936470c4ba289
danswick <dan.swick@gmail.com>
2015-10-20 18:18:23 -0700

    initial commit

---
+++ app/src/main/java/com/mycompany/myfirstglapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.mycompany.myfirstglapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** Create a mapView and give it some properties */
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mapView.setCenterCoordinate(new LatLng(41.885, -87.679));
+        mapView.setZoomLevel(9);
+
+        mapView.addMarker(new MarkerOptions()
+                .position(new LatLng(41.885, -87.679))
+                .title("Hello World!")
+                .snippet("Welcome to my marker.")
+                .sprite("secondary_marker"));
+
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause()  {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
