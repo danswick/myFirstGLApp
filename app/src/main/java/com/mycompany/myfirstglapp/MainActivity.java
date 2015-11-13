c2e25f626a38f1129c55608cb6f550b66cd6fce2
danswick <dan.swick@gmail.com>
2015-11-13 12:04:24 -0800

    update to latest marker add meethod

--- app/src/main/java/com/mycompany/myfirstglapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myfirstglapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.mycompany.myfirstglapp;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
+import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.Sprite;
+import com.mapbox.mapboxsdk.annotations.SpriteFactory;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
@@ -24,11 +29,15 @@ public class MainActivity extends AppCompatActivity {
         mapView.setCenterCoordinate(new LatLng(41.885, -87.679));
         mapView.setZoomLevel(9);
 
+        SpriteFactory spriteFactory = new SpriteFactory(mapView);
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_directions_boat_black_18dp);
+        Sprite icon = spriteFactory.fromDrawable(drawable);
+
         mapView.addMarker(new MarkerOptions()
                 .position(new LatLng(41.885, -87.679))
                 .title("Hello World!")
                 .snippet("Welcome to my marker.")
-                .sprite("secondary_marker"));
+                .icon(icon));
 
         mapView.onCreate(savedInstanceState);
     }
